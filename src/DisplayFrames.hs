c086ffbe2a8966fc29f620342dbb80dc84adb115
Kit Freddura <kit@theagilemonkeys.com>
2017-03-06 10:10:31 -0800

    made records render as table entries

--- src/DisplayFrames.hs
+++ src/DisplayFrames.hs
@@ -6,14 +6,15 @@ import DisplayTypes
 import Displayable
 import Frames
 import Data.Vinyl.TypeLevel (RecAll)
-import Text.Blaze.Html5 (toHtml)
+import Text.Blaze.Html5 (toHtml, td, tr)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import Data.Text.Lazy (toChunks)
 import qualified Data.Vinyl.Functor as V
 import qualified Data.Text as T
 
 -- instance of Displayable of Record from Frames package
-instance (RecAll V.Identity r Show, ColumnHeaders r, AsVinyl r) => Displayable (Record r) where
+instance (RecAll V.Identity (UnColumn r) Show, ColumnHeaders r, AsVinyl r) => Displayable (Record r) where
   display rec = Display DisplayHtml html 
     where 
-      html = (T.unpack . T.concat . toChunks . renderHtml) (toHtml $ show rec)
+      html = renderHtml $ toTable $ showFields rec
+      toTable = tr . mapM_ (td . toHtml)
